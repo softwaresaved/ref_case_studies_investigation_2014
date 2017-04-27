4a0cb548f9d24f7f44eb4d43e96f9feb282c31f9
simonhettrick <s.hettrick@software.ac.uk>
2017-04-27 11:54:12 +0100

    Now reads data by searching for xls files rather than having their names spoonfed to it

--- organise_studies_by_funder.py
+++ organise_studies_by_funder.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from pandas import ExcelWriter
 import numpy as np
+import os.path
 #import matplotlib.pyplot as plt
 import math
 
@@ -13,33 +14,33 @@ OUTPUT = "./data/list_of_studies_by_council.xlsx"
 
 
 def read_data():
-    """
-    Read in spreadsheets of case studies for each council. Knock out all information
-    except the case study ID. Combine this information into a dataframe of case
-    study ID and associated funder
+    '''
+    Goes through all the xlsx files in the appropriate dir, converts each
+    one to a df and concatenates successively into a super df with all
+    the data
     :params: none
-    :return: a dataframe with case study IDs and associated funder
-    """
+    :return: a dataframe formed from multiple dataframes
+    '''
+    new = True
+    for file in os.listdir(DATA_FILE_DIR):
+        if file.endswith('.xlsx'):
+            # Create the col name by dropping the file extension from the filename,
+            # and then adding the keyword "funder_"
+            new_col = 'funder_' + str(file)[:-5]
 
-    # Create a list of the funders available in the data, which corresponds
-    # to the names of the spreadsheets that hold the data from each funder
-    funder_list = ['ahrc', 'bbsrc', 'british_academy', 'cclrc', 'epsrc', 'esrc',
-                   'mrc', 'nerc', 'pparc', 'rcuk', 'royal_academy_engineering', 
-                   'royal_society', 'stfc', 'uk_space_agency', 'wellcome']
-
-    # Initiate a dataframe in which to store the results
-    dataframe = pd.DataFrame(np.nan, index=[0], columns=['Case Study Id'])
-
-    # Cycle through the funder spreadsheets and create a main dataframe containing
-    # all case studies and the associated funder(s)
-    for current in funder_list:
-        df = pd.read_excel(DATA_FILE_DIR + current + '.xlsx', sheetname='CaseStudies')
-        # Knock out everything except the case study
-        df = pd.DataFrame(df['Case Study Id'])
-        # Add a column which is the name of the funder
-        df[current] = current
-        # Use concat to add current dataframe onto end of the main dataframe 
-        dataframe = pd.concat([dataframe, df])
+            #Need this if loop because the first iternation has to be a straight read rather than a concatenation
+            if new == True:
+                dataframe = pd.read_excel(DATA_FILE_DIR + str(file), sheetname='CaseStudies')
+                dataframe = pd.DataFrame(dataframe['Case Study Id'])
+                dataframe[new_col] = new_col
+                new = False
+            else:
+                temp_df = pd.read_excel(DATA_FILE_DIR + str(file), sheetname='CaseStudies')
+                # Knock out everything except the case study
+                temp_df = pd.DataFrame(temp_df['Case Study Id'])
+                temp_df[new_col] = new_col
+                # Use concat to add temp dataframe onto end of the main dataframe 
+                dataframe = pd.concat([dataframe, temp_df])
 
     return dataframe
 
