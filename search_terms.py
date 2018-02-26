cf5a2254f0cb7c7ee0883b9967932731ff85b7bd
simonhettrick <s.hettrick@software.ac.uk>
2018-02-26 12:14:53 +0000

    Search terms now pulled from a look up file

---
+++ search_terms.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Here's the list of words used to search the REF case studies
+"""
+
+SEARCH_TERM_LIST = [
+    'big data',
+    'computation',
+    'computational',
+    'computed',
+    'computer',
+    'data analysis',
+    'data base',
+    'database',
+    'excel',
+    'fortran',
+    'geographic information science',
+    'geographic information systems',
+    'gis',
+    'git',
+    'github',
+    'graphics',
+    'high performance computing',
+    'hpc',
+    'imagej',
+    'in silico',
+    'matlab',
+    'model',
+    'modeling',
+    'modelling',
+    'nvivo',
+    'python',
+    'quantitative',
+    'r language',
+    'in r',
+    'simulated',
+    'simulation',
+    'software',
+    'spss',
+    'stata',
+    'supercomputing',
+    'visualisation',
+    'visualization'
+    ]
