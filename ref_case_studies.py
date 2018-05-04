f7e0e4d7488d30feaf3ef89c098a03a2aea2f950
steve-crouch <s.crouch@software.ac.uk>
2018-05-04 14:44:47 +0100

    Add funders chart, some PEP8 cleanup, rerun with updated search terms

--- ref_case_studies.py
+++ ref_case_studies.py
@@ -2,11 +2,7 @@
 # encoding: utf-8
 
 import pandas as pd
-from pandas import ExcelWriter
-import numpy as np
 import matplotlib.pyplot as plt
-import math
-import re
 
 from search_terms import SEARCH_TERM_LIST
 
@@ -22,22 +18,13 @@ RESULT_STORE = "./outputs/"
 CHART_RESULT_STORE = "./outputs/charts/"
 
 
-def import_xls_to_df(filename, name_of_sheet):
-    """
-    Imports an Excel file into a Pandas dataframe
-    :params: get an xls file and a sheetname from that file
-    :return: a df
-    """
-    return pd.read_excel(filename,sheetname=name_of_sheet)
-
-
 def import_csv_to_df(filename):
     """
     Imports a csv file into a Pandas dataframe
     :params: a csv file
     :return: a df
     """
-    
+
     return pd.read_csv(filename)
 
 
@@ -51,39 +38,23 @@ def export_to_csv(df, location, filename):
     return df.to_csv(location + filename + '.csv')
 
 
-def write_results_to_xls(dataframe, title):
-    """
-    Takes a dataframe and writes it to an Excel spreadsheet based on a string
-    which describes the save location and title
-    :params: a dataframe, a string containing desired location and title of a Excel spreadsheet
-    :return: nothing (writes an Excel spreadsheet)
-    """
-    
-    filename = title.replace(" ", "_")
-
-    writer = ExcelWriter(EXCEL_RESULT_STORE + filename + '.xlsx')
-    # Write result to Excel
-    dataframe.to_excel(writer, 'Sheet1')
-    # Close Excel writer
-    writer.save()
-
-    return
-
-
 def cut_to_specific_word(dataframe, specific_word, part_in_bid):
     """
     Takes in a dataframe and a column, and then creates a new dataframe containing only
     the rows from the original dataframe that had the search word in that column
     :params: a dataframe, a specific_word to look for, and a column name relating to
              a part in a case study in which the specific_word should be searched for
-    :return: a dataframe containing only the case studies which had specific word in 
+    :return: a dataframe containing only the case studies which had specific word in
              the column of interest
     """
 
     # Cut dataframe down to only those rows with a word in the right column
     current_df = dataframe[dataframe[part_in_bid].str.contains(r'\b' + specific_word + r'\b', regex=True, na=False)]
+    # Suppress the annoyingly pedantic SettingWithCopyWarning,
+    # since we know explicitly we're dealing with a copy
+    current_df.is_copy = False
     # Add a new col to indicate where the specific word was found
-    new_col_name = specific_word +'_found_in_' + part_in_bid
+    new_col_name = specific_word + '_found_in_' + part_in_bid
     current_df[new_col_name] = part_in_bid
     # Drop all columns except the case study and the col showing where the word was found
     current_df = current_df[['Case Study Id', new_col_name]]
@@ -101,27 +72,29 @@ def merge_search_place(dataframe, df_cut):
     :return: a dataframe created by merging the two dataframes
     """
 
-    dataframe = pd.merge(left=dataframe,right=df_cut, how='left', left_on='Case Study Id', right_on='Case Study Id')
+    dataframe = pd.merge(left=dataframe, right=df_cut, how='left', left_on='Case Study Id', right_on='Case Study Id')
 
     return dataframe
 
 
 def associate_new_data(dataframe, df_studies_by_funder):
-    """
+    """Merge two dataframes based on Case Study ID.
+
     Takes a dataframe with the case study information and merges it with another
     dataframe that contains case study IDs and some other data (e.g. funders, disciplines)
+
     :params: a dataframe with case study information, a second dataframe with cases study IDs and other information
     :return: a dataframe containing case study information and other information
     """
-    
-    dataframe = pd.merge(left=dataframe,right=df_studies_by_funder, how='left', left_on='Case Study Id', right_on='Case Study Id')
-        
+
+    dataframe = pd.merge(left=dataframe, right=df_studies_by_funder, how='left', left_on='Case Study Id', right_on='Case Study Id')
+
     return dataframe
 
 
 def get_col_list(df, search_string):
-    """
-    Find all cols with a specific string in them
+    """Find all cols with a specific string in them.
+
     :return: a list of cols
     """
     list_cols = []
@@ -134,9 +107,7 @@ def get_col_list(df, search_string):
 
 
 def summarise_search_terms(df, search_places, cols_list, all_case_study_count):
-    """
-    Summarise the results across all words searched for
-    
+    """Summarise the results across all words searched for.
 
     :returns: a dataframe with the summary results
     """
@@ -153,9 +124,9 @@ def summarise_search_terms(df, search_places, cols_list, all_case_study_count):
         # Drop all rows where NaN across the "matching" list
         temp_df = df.dropna(subset=[matching], how='all', axis=0)
         summary_data[curr_place] = len(temp_df)
-    
-    summary_df = pd.DataFrame(list(summary_data.items()), columns=['word location', 'count matching 1 word'])    
-    summary_df['% of all studies'] = round(100 * (summary_df['count matching 1 word']/all_case_study_count),0)
+
+    summary_df = pd.DataFrame(list(summary_data.items()), columns=['word location', 'count matching 1 word'])
+    summary_df['% of all studies'] = round(100 * (summary_df['count matching 1 word']/all_case_study_count), 0)
     summary_df.sort_values(['count matching 1 word'], ascending=False, inplace=True)
 
     # Now that we've sorted the count for a single word found in the df
@@ -168,35 +139,37 @@ def summarise_search_terms(df, search_places, cols_list, all_case_study_count):
             temp_df = shortened_df.dropna(subset=[matching], how='all', axis=0)
             count_plus_i_word[curr_place] = len(temp_df)
         summary_df['count matching ' + str(i) + ' words'] = summary_df['word location'].map(count_plus_i_word)
-        summary_df['% all studies ' + str(i) + ' words'] = round(100 * (summary_df['count matching ' + str(i) + ' words']/all_case_study_count),0)
-        
+        summary_df['% all studies ' + str(i) + ' words'] = round(100 * (summary_df['count matching ' + str(i) + ' words']/all_case_study_count), 0)
+
     summary_df.set_index('word location', inplace=True)
-    
+
     return summary_df
 
 
 def summarise_funders(df, cols_to_search, all_case_study_count):
-        
+    """Create a summary df of the funders found in the data."""
+
     # Create temp df containing only the cols to be searched and use count() to create a summary series
     temp_df = df[cols_to_search]
-    s = temp_df.count()
-    
+    count_series = temp_df.count()
+
     # Convert summary series into df
-    df_summary = pd.DataFrame({'index':s.index, 'count':s.values})
-    df_summary.set_index('index', inplace=True)
-    df_summary.sort_values(['count'], ascending=False, inplace=True)
+    summary_df = pd.DataFrame({'index': count_series.index, 'count': count_series.values})
+    summary_df.set_index('index', inplace=True)
+    summary_df.sort_values(['count'], ascending=False, inplace=True)
 
     # Add a percentage col
-    df_summary['percentage of all studies'] = round((df_summary['count']/all_case_study_count)*100,0)
+    summary_df['% of all studies'] = round((summary_df['count']/all_case_study_count)*100, 0)
 
-    return df_summary
+    # Remove funder prefix and underscores from index (if they exist)
+    summary_df.index = summary_df.index.map(lambda x: x.replace('funder', '').replace('_', ' ') if isinstance(x, str) else x)
+
+    return summary_df
 
 
 def summarise_uoas(df, df_term_found, list_of_uoas, all_case_study_count):
-    """
-    Create a summary df of the number of Units of Assessment found in the data
-    """
-    
+    """Create a summary df of the number of Units of Assessment found in the data."""
+
     uoa_term_found_dict = {}
     uoa_all_dict = {}
 
@@ -209,33 +182,35 @@ def summarise_uoas(df, df_term_found, list_of_uoas, all_case_study_count):
         uoa_all_dict[current_uoa] = len(temp_2_df)
 
     # Create a df from the dict
-    summary_df = pd.DataFrame(list(uoa_term_found_dict.items()), columns=['unit of assessment', 'software reliant count'])    
+    summary_df = pd.DataFrame(list(uoa_term_found_dict.items()), columns=['unit of assessment', 'software reliant count'])
     # Add a column to the summary from the uoa_all_dict
     summary_df['all studies count'] = summary_df['unit of assessment'].map(uoa_all_dict)
     summary_df.set_index('unit of assessment', inplace=True)
 
-    summary_df['percentage of studies in this uoa'] = round(100 * (summary_df['software reliant count']/summary_df['all studies count']),0)
-    summary_df['percentage of all studies'] = round(100 * (summary_df['software reliant count']/all_case_study_count),0)
+    summary_df['percentage of studies in this uoa'] = round(100 * (summary_df['software reliant count']/summary_df['all studies count']), 0)
+    summary_df['percentage of all studies'] = round(100 * (summary_df['software reliant count']/all_case_study_count), 0)
     summary_df.sort_values(['software reliant count'], ascending=False, inplace=True)
 
     return summary_df
 
+
 def summarise_word_popularity(df, all_case_study_count):
-    
+    """Create a summary df of the count of search terms found in the data."""
+
     # Get a list of all the found_in columns
     matching = [s for s in df.columns if 'found_in' in s]
     # Remove the any_term part, because it's a summary that we don't need
     matching.remove('any_term_found_in_anywhere')
-    
+
     matches_to_search_term = {}
-    
+
     for curr_term in SEARCH_TERM_LIST:
         curr_term_match = [s for s in matching if curr_term in s]
         temp_df = df.dropna(subset=[curr_term_match], how='all', axis=0)
         matches_to_search_term[curr_term] = len(temp_df)
 
-    summary_df = pd.DataFrame(list(matches_to_search_term.items()), columns=['search term', 'count'])    
-    summary_df['% of all studies'] = round(100 * (summary_df['count']/all_case_study_count),0)
+    summary_df = pd.DataFrame(list(matches_to_search_term.items()), columns=['search term', 'count'])
+    summary_df['% of all studies'] = round(100 * (summary_df['count']/all_case_study_count), 0)
     summary_df.sort_values(['count'], ascending=False, inplace=True)
     summary_df.set_index('search term', inplace=True)
 
@@ -243,13 +218,12 @@ def summarise_word_popularity(df, all_case_study_count):
 
 
 def plot_bar_from_df(df, y_col, title, x_axis_title, y_axis_title):
-    """
-    Plot a functional, rather than a pretty, chart
+    """Plot a functional, rather than a pretty, chart from a dataframe.
+
     Pretty charts can be made in the "graphing for presentations code" repo in Github
     """
 
-
-    ax = df.plot(y = y_col, kind='bar', legend=None)
+    ax = df.plot(y=y_col, kind='bar', legend=None)
     for i, each in enumerate(df.index):
         y = df.ix[each][y_col]
         ax.text(i-0.3, y+0.2, int(y))
@@ -264,23 +238,17 @@ def plot_bar_from_df(df, y_col, title, x_axis_title, y_axis_title):
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
-
-    labels = df[y_col].tolist()
-    
-    # This provides more space around the chart to make it prettier        
+    # This provides more space around the chart to make it prettier
     plt.tight_layout(True)
-    filename = title.replace(" ", "_")
-    plt.savefig(CHART_RESULT_STORE + filename + '.png', format = 'png', dpi = 150)
-    plt.show()
-    
+    filename = title.replace(' ', '_')
+    plt.savefig(CHART_RESULT_STORE + filename + '.png', format='png', dpi=150)
+
     return
 
 
-
 def main():
-    """
-    Main function to run program
-    
+    """Main function to run program.
+
     To change the word searched for in the case studies,
     change the global variable found at the very start of
     the program called WORD_TO_SEARCH_FOR
@@ -293,7 +261,7 @@ def main():
 
     # Import case study data
     df = import_csv_to_df(DATAFILENAME)
-    
+
     # Import case studies by funder
     # This is only used to create a list of funders
     df_studies_by_funder = import_csv_to_df(STUDIES_BY_FUNDER)
@@ -308,9 +276,6 @@ def main():
     # Create a list of the units of assessment
     list_of_uoas = list(df_uoas['Unit of assessment'].str.lower())
     list_of_uoas.sort()
-
-    #Need this list later: used to remove columns relating to original data
-    original_cols = list(df.columns)
 
     # Record length of original df and hence, number of all case studies
     all_case_study_count = len(df)
@@ -339,7 +304,6 @@ def main():
     temp = df['search terms found'].unique().tolist()
     temp.sort()
 
-
     # Add anywhere to the search places, because it's an addition that's not in
     # the original list
     found_in_cols.append('any_term_found_in_anywhere')
@@ -350,7 +314,7 @@ def main():
 
     # Summarise the data across search terms or where they were found
     df_summary_terms = summarise_search_terms(df_term_identified, possible_search_places, found_in_cols, all_case_study_count)
-    
+
     # Get a summary of the data across funders
     df_summary_funders = summarise_funders(df_term_identified, funder_cols, all_case_study_count)
 
@@ -360,14 +324,16 @@ def main():
 
     df_summary_popularity = summarise_word_popularity(df, all_case_study_count)
 
-    # Write out to Excel
-    export_to_csv(df_term_identified, RESULT_STORE, 'only_case_studies_with_search_term_identified')   
+    # Write results to CSV files
+    export_to_csv(df_term_identified, RESULT_STORE, 'only_case_studies_with_search_term_identified')
     export_to_csv(df_summary_terms, RESULT_STORE, 'summary_of_where_terms_found')
     export_to_csv(df_summary_funders, RESULT_STORE, 'summary_of_funders')
     export_to_csv(df_summary_uoas, RESULT_STORE, 'summary_of_uoas')
     export_to_csv(df_summary_popularity, RESULT_STORE, 'summary_of_word_popularity')
 
-    plot_bar_from_df(df_summary_popularity, '% of all studies', 'Incidence of search words in REF 2014 case studies', '', 'As percentage of all case studies')
+    # Generate PNG charts from our results
+    plot_bar_from_df(df_summary_popularity, '% of all studies', 'Incidence of search words in REF 2014 case studies', '', '% of all case studies')
+    plot_bar_from_df(df_summary_funders, '% of all studies', 'Case studies by funder', '', '% of all case studies')
 
 
 if __name__ == '__main__':
