c5a041a584318c6ce89e3323eb15a8fd0cc380a1
steve-crouch <s.crouch@software.ac.uk>
2018-05-08 14:47:37 +0100

    Improve PEP8 compliance, remove unnecessary try/except, minor cleanup

--- merge_studies_with_funder.py
+++ merge_studies_with_funder.py
@@ -2,75 +2,35 @@
 # encoding: utf-8
 
 import pandas as pd
-from pandas import ExcelWriter
 
 # Other global variables
 DATAFILENAME = "input/raw/CaseStudies.xlsx"
 STUDIES_BY_FUNDER = "input/generated/list_of_studies_by_council.csv"
 RESULT_STORE = "input/generated/"
 
+
 def import_xls_to_df(filename, name_of_sheet):
-    """
-    Imports an Excel file into a Pandas dataframe
+    """Imports an Excel file into a Pandas dataframe.
+
     :params: get an xls file and a sheetname from that file
     :return: a df
     """
-    return pd.read_excel(filename,sheetname=name_of_sheet)
+    return pd.read_excel(filename, sheetname=name_of_sheet)
 
 
 def import_csv_to_df(filename):
-    """
-    Imports a csv file into a Pandas dataframe
+    """Imports a csv file into a Pandas dataframe.
+
     :params: a csv file
     :return: a df
     """
-    
+
     return pd.read_csv(filename)
 
 
-def clean(dataframe):
-    """
-    Cleans the imported data for easy processing by removing end of lines chars, multiple spaces,
-    and lowercasing everying
-    :params: a dataframe
-    :return: a dataframe with clean data
-    """
-
-    # Someone thought it would be a good idea to add line breaks to the longer strings in Excel
-    # This removes them 
-    dataframe = dataframe.replace(to_replace='\n', value=' ', regex=True)
-
-    # There are also multiple spaces in the strings. This removes them.
-    dataframe = dataframe.replace('\s+', ' ', regex=True)
-    
-    # And now to remove the leading spaces and lowercase everything
-    # Need the try and except because some of the cols have integers
-    for col in dataframe.columns:
-        try:
-            dataframe[col] = dataframe[col].map(lambda x: x.strip())
-            dataframe[col] = dataframe[col].str.lower()
-        except:
-            pass
-            
-    return dataframe
-
-
-def associate_new_data(dataframe, df_studies_by_funder):
-    """
-    Takes a dataframe with the case study information and merges it with another
-    dataframe that contains case study IDs and some other data (e.g. funders, disciplines)
-    :params: a dataframe with case study information, a second dataframe with cases study IDs and other information
-    :return: a dataframe containing case study information and other information
-    """
-    
-    dataframe = pd.merge(left=dataframe,right=df_studies_by_funder, how='left', left_on='Case Study Id', right_on='Case Study Id')
-        
-    return dataframe
-
-
 def export_to_csv(df, location, filename):
-    """
-    Exports a df to a csv file
+    """Exports a df to a csv file.
+
     :params: a df and a location in which to save it
     :return: nothing, saves a csv
     """
@@ -78,15 +38,46 @@ def export_to_csv(df, location, filename):
     return df.to_csv(location + filename + '.csv')
 
 
-def main():
-    """
-    Main function to run program
-    
-    To change the word searched for in the case studies,
-    change the global variable found at the very start of
-    the program called WORD_TO_SEARCH_FOR
+def clean(dataframe):
+    """Cleans the imported data for easy processing.
+
+    Removes end of lines chars, multiple spaces, and lowercases everything
+
+    :params: a dataframe
+    :return: a dataframe with clean data
     """
 
+    # Someone thought it would be a good idea to add line breaks
+    # to the longer strings in Excel - this removes them
+    dataframe = dataframe.replace(to_replace='\n', value=' ', regex=True)
+
+    # There are also multiple spaces in the strings - this removes them
+    dataframe = dataframe.replace('\s+', ' ', regex=True)
+
+    # And now to remove the leading spaces and lowercase everything
+    # Need to do this conditionally since some of the cols have integers
+    for col in dataframe.columns:
+        dataframe[col] = dataframe[col].map(lambda x: x.strip().lower() if isinstance(x, str) else x)
+
+    return dataframe
+
+
+def associate_new_data(dataframe, df_studies_by_funder):
+    """Merge two dataframes based on Case Study ID.
+
+    Takes a dataframe with the case study information and merges it with another
+    dataframe that contains case study IDs and some other data (e.g. funders, disciplines)
+
+    :params: a dataframe with case study information, a second dataframe with cases study IDs and other information
+    :return: a dataframe containing case study information and other merged information
+    """
+
+    dataframe = pd.merge(left=dataframe, right=df_studies_by_funder, how='left', left_on='Case Study Id', right_on='Case Study Id')
+
+    return dataframe
+
+
+def main():
     # Import dataframe from original xls
     df = import_xls_to_df(DATAFILENAME, 'CaseStudies')
 
@@ -95,12 +86,13 @@ def main():
 
     # Import case study ids for each funder
     df_studies_by_funder = import_csv_to_df(STUDIES_BY_FUNDER)
-    
+
     # Associate case study IDs with specific funders
     df = associate_new_data(df, df_studies_by_funder)
 
     # Export results
     export_to_csv(df, RESULT_STORE, 'all_ref_case_study_data')
+
 
 if __name__ == '__main__':
     main()
